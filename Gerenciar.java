babd391aa6ec33ce7a3d6dee51f4bec2d256be47
vinicius.dev <72517355+viniciusdevegili@users.noreply.github.com>
2024-03-29 17:50:31 -0300

    .

--- Gerenciar.java
+++ Gerenciar.java
@@ -1,98 +1,101 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Gerenciar {
     public static void main(String[] args) {
         List<Item> listaItens = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        int op;
         String nomeItem = "";
+        int op = 0;
 
-        System.out.println("Escolha a opção desejada: ");
-        System.out.println(" 1  - Adicionar novo item");
-        System.out.println(" 2  - Alterar quantidade do item");
-        System.out.println(" 3  - Alterar valor do item");
-        System.out.println(" 4  - Valor total em estoque");
+        while (op != 4) {
+            System.out.println("Escolha a opção desejada: ");
+            System.out.println(" 1  - Adicionar novo item");
+            System.out.println(" 2  - Alterar quantidade do item");
+            System.out.println(" 3  - Alterar valor do item");
+            System.out.println(" 4  - Valor total em estoque");
 
-        op = sc.nextInt();
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    sc.nextLine();
+                    System.out.println("Digite o nome do novo item: ");
+                    nomeItem = sc.nextLine();
 
-        switch (op) {
-            case 1:
-                sc.nextLine(); // Limpar o buffer do scanner
-                System.out.println("Digite o nome do novo item: ");
-                nomeItem = sc.nextLine();
+                    System.out.println("Digite a quantidade do novo item: ");
+                    int quantidade = sc.nextInt();
 
-                System.out.println("Digite a quantidade do novo item: ");
-                int quantidade = sc.nextInt();
+                    System.out.println("Digite o valor do novo item: ");
+                    double valor = sc.nextDouble();
 
-                System.out.println("Digite o valor do novo item: ");
-                double valor = sc.nextDouble();
+                    Item novoItem = new Item(nomeItem, quantidade, valor);
+                    listaItens.add(novoItem);
 
-                // Criar um novo item e adicioná-lo à lista de itens
-                Item novoItem = new Item(nomeItem, quantidade, valor);
-                listaItens.add(novoItem);
+                    System.out.println("Item adicionado com sucesso!");
+                    break;
 
-                System.out.println("Item adicionado com sucesso!");
-                break;
+                case 2:
+                    System.out.println("Digite o nome do item que deseja alterar a quantidade: ");
+                    sc.nextLine();
+                    String nomeItemAlterar = sc.nextLine();
 
-            case 2:
-                System.out.println("Digite o nome do item que deseja alterar a quantidade: ");
-                sc.nextLine();
-                String nomeItemAlterar = sc.nextLine();
+                    boolean itemEncontrado = false;
+                    for (Item item : listaItens) {
+                        if (item.getNomeItem().equalsIgnoreCase(nomeItemAlterar)) {
+                            itemEncontrado = true;
 
-                boolean itemEncontrado = false;
-                for (Item item : listaItens) {
-                    if (item.getNomeItem().equalsIgnoreCase(nomeItemAlterar)) {
-                        itemEncontrado = true;
+                            System.out.println("Digite a nova quantidade para o item '" + nomeItemAlterar + "': ");
+                            int novaQuantidade = sc.nextInt();
 
-                        System.out.println("Digite a nova quantidade para o item '" + nomeItemAlterar + "': ");
-                        int novaQuantidade = sc.nextInt();
+                            item.setQuantidade(novaQuantidade);
 
-                        item.setQuantidade(novaQuantidade);
-
-                        System.out.println(
-                                "Quantidade do item '" + nomeItemAlterar + "' alterada para " + novaQuantidade);
-                        break;
+                            System.out.println(
+                                    "Quantidade do item '" + nomeItemAlterar + "' alterada para " + novaQuantidade);
+                            break;
+                        }
                     }
-                }
 
-                if (!itemEncontrado) {
-                    System.out.println("Item não encontrado.");
-                }
-                break;
-
-            case 3:
-                System.out.println("Digite o nome do item que deseja alterar o valor: ");
-                sc.nextLine();
-                String nomeItemAlterarValor = sc.nextLine();
-
-                boolean itemEncontradoValor = false;
-                for (Item item : listaItens) {
-                    if (item.getNomeItem().equalsIgnoreCase(nomeItemAlterarValor)) {
-                        itemEncontradoValor = true;
-
-                        System.out.println("Digite o novo valor para o item '" + nomeItemAlterarValor + "': ");
-                        double novoValor = sc.nextDouble();
-
-                        item.setValor(novoValor);
-
-                        System.out.println("Valor do item '" + nomeItemAlterarValor + "' alterado para " + novoValor);
-                        break;
+                    if (!itemEncontrado) {
+                        System.out.println("Item não encontrado.");
                     }
-                }
+                    break;
 
-                if (!itemEncontradoValor) {
-                    System.out.println("Item não encontrado.");
-                }
-                break;
-            case 4:
+                case 3:
+                    System.out.println("Digite o nome do item que deseja alterar o valor: ");
+                    sc.nextLine();
+                    String nomeItemAlterarValor = sc.nextLine();
 
-                break;
+                    boolean itemEncontradoValor = false;
+                    for (Item item : listaItens) {
+                        if (item.getNomeItem().equalsIgnoreCase(nomeItemAlterarValor)) {
+                            itemEncontradoValor = true;
 
-            default:
-                System.out.println("Você inseriu uma opção inválida.");
+                            System.out.println("Digite o novo valor para o item '" + nomeItemAlterarValor + "': ");
+                            double novoValor = sc.nextDouble();
+
+                            item.setValor(novoValor);
+
+                            System.out
+                                    .println("Valor do item '" + nomeItemAlterarValor + "' alterado para " + novoValor);
+                            break;
+                        }
+                    }
+
+                    if (!itemEncontradoValor) {
+                        System.out.println("Item não encontrado.");
+                    }
+                    break;
+                case 4:
+                    double totalEmEstoque = 0.0;
+                    for (Item item : listaItens) {
+                        totalEmEstoque += item.getQuantidade() * item.getValor();
+                    }
+                    System.out.println("O valor total em estoque é: R$" + totalEmEstoque);
+                    break;
+
+                default:
+                    System.out.println("Você inseriu uma opção inválida.");
+            }
         }
     }
 }
